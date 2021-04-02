d50ac0b60330aaf60176f11a1a003fcaea411cbf
camha91 <crystalhanguyen@gmail.com>
2021-04-01 20:52:55 -0700

    Updated UI

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,73 +1,121 @@
 import React, { Component } from "react";
+import "./Calculator.css";
 
 export default class Calculator extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div classname="row">
-          <div
-            style={{ width: "500px" }}
-            className="card text-white bg-default mt-5"
-          >
-            <div className="card-body">
-              <div className="row-10">
-                <div className="col mt-5">
-                  <div className="input-group mb-3">
-                    <input
-                      type="number"
-                      className="form-control"
-                      aria-label="Default"
-                      aria-describedby="inputGroup-sizing-default"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="row-10">
-                <div className="col-9">
-                  <div className="row">
-                    <div className="col-6">
-                      <button type="button" class="btn btn-default border mr-2">
-                        7
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        8
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        9
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        4
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        5
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        6
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        1
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        2
-                      </button>
-                      <button type="button" class="btn btn-default border mr-2">
-                        3
-                      </button>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-6">
-                      <button type="button" class="btn btn-default border mr-2">
-                        0
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-3"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container">
+        <input type="text" className="display" disabled />
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  7
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  8
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  9
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  /
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  CE
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  4
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  5
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  6
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  *
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  C
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  1
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  2
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  3
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  -
+                </button>
+              </td>
+              <td rowSpan="2" height="50px">
+                <button
+                  style={{ width: "100%", height: "100%" }}
+                  type="button"
+                  class="btn btn-default border"
+                >
+                  =
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <button
+                  style={{ width: "100%" }}
+                  type="button"
+                  class="btn btn-default border"
+                >
+                  0
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  .
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default border">
+                  +
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
